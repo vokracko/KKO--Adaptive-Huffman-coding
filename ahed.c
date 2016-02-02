261881401595bd71909580a1bf8d6c5c07f66c9f
Lukáš Vokráčko <lukas@vokracko.cz>
2016-02-02 12:01:11 +0100

    init

---
+++ ahed.c
@@ -0,0 +1,45 @@
+/*
+ * Autor: Jmeno Prijmeni (login)
+ * Datum:
+ * Soubor:
+ * Komentar:
+ */ 
+
+#include "ahed.h"
+
+/* Nazev:
+ *   AHEDEncoding
+ * Cinnost:
+ *   Funkce koduje vstupni soubor do vystupniho souboru a porizuje zaznam o kodovani.
+ * Parametry:
+ *   ahed - zaznam o kodovani
+ *   inputFile - vstupni soubor (nekodovany)
+ *   outputFile - vystupní soubor (kodovany)
+ * Navratova hodnota: 
+ *    0 - kodovani probehlo v poradku
+ *    -1 - pøi kodovani nastala chyba
+ */
+int AHEDEncoding(tAHED *ahed, FILE *inputFile, FILE *outputFile)
+{
+	return AHEDOK;
+}
+
+/* Nazev:
+ *   AHEDDecoding
+ * Cinnost:
+ *   Funkce dekoduje vstupni soubor do vystupniho souboru a porizuje zaznam o dekodovani.
+ * Parametry:
+ *   ahed - zaznam o dekodovani
+ *   inputFile - vstupni soubor (kodovany)
+ *   outputFile - vystupní soubor (nekodovany)
+ * Navratova hodnota: 
+ *    0 - dekodovani probehlo v poradku
+ *    -1 - pøi dekodovani nastala chyba
+ */
+int AHEDDecoding(tAHED *ahed, FILE *inputFile, FILE *outputFile)
+{
+	return AHEDOK;
+}
+
+
+
---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Autor: Jmeno Prijmeni (login)
+ * Datum:
+ * Soubor:
+ * Komentar:
+ */ 
+#include <stdlib.h>
+#include "ahed.h"
+
+
+int main(int argc, char **argv)
+{
+
+	exit(0);
+}
+
+
+	
