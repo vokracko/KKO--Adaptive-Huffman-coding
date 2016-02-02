b99c62dbb348f520f8b2d50da15d413b8d53ea34
Lukáš Vokráčko <lukas@vokracko.cz>
2016-02-02 14:31:34 +0100

    handle parameters

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int open_file(FILE ** file, const char * mode, const char * path)
 	*file = fopen(path, mode);
 
 	if(*file < 0)
-		return -1;
+		return 0;
 
 	return 1;
 
@@ -39,11 +39,12 @@ int open_file(FILE ** file, const char * mode, const char * path)
 
 int main(int argc, char **argv)
 {
-	FILE * ofile = stdout;
-	FILE * ifile = stdin;
-	FILE * logfile;
+	int (*f)(tAHED *, FILE *, FILE*) = NULL;
+	FILE * ofile = NULL;
+	FILE * ifile = NULL;
+	FILE * logfile = NULL;
+	tAHED ahed;
 	char c;
-	int mode;
 	int res;
 
 	while((c = getopt(argc, argv, "i:o:l:cxh")) != -1)
@@ -51,19 +52,19 @@ int main(int argc, char **argv)
 		switch(c) 
 		{
 			case 'i':
-				open_file(&ifile, "r", optarg);
+				open_file(&ifile, "r", optarg) || printf("Failed to open %s\n", optarg), res -1;
 				break;
 			case 'o':
-				open_file(&ofile, "w+", optarg);
+				open_file(&ofile, "w+", optarg) || printf("Failed to open %s\n", optarg), res -1;
 				break;
 			case 'l':
-				open_file(&logfile, "w+", optarg);
+				open_file(&logfile, "w+", optarg) || printf("Failed to open %s\n", optarg), res -1;
 				break;
 			case 'c':
-				mode = CODE;
+				f = AHEDEncoding;
 				break;
 			case 'x':
-				mode = DECODE;
+				f = AHEDDecoding;
 				break;
 			case 'h':
 				print_help();
@@ -71,7 +72,39 @@ int main(int argc, char **argv)
 		}
 	}
 
-	return res;
+	if(res != -1)
+	{
+
+		if(ifile == NULL)
+			ifile = stdin;
+
+		if(ofile == NULL)
+			ofile = stdout;
+
+		if(f == NULL)
+		{
+			puts("No mode specified");
+			print_help();
+			res = -1;
+		}
+		else
+		{
+			res = f(&ahed, ifile, ofile);
+		}
+	}
+
+	if(res > 0 && logfile)
+	{
+		fprintf(logfile, "login = xvokra00\n");
+		fprintf(logfile, "uncodedSize = %d\n", ahed.uncodedSize);
+		fprintf(logfile, "codedSize = %d\n", ahed.codedSize);
+	}
+
+	fclose(ifile);
+	fclose(ofile);
+	fclose(logfile);
+
+	return res * -1;
 }
 
 
